0094734249450d602df90afb9dc213b0c29c3f4a
michaelschoolap <52840616+michaelmogolo@users.noreply.github.com>
2024-12-27 19:41:33 +0100

    mise à jour des importations dans HappyNewYear.vue pour corriger le chemin d'OrbitControls

--- src/views/HappyNewYear.vue
+++ src/views/HappyNewYear.vue
@@ -28,11 +28,10 @@
 </template>
 
 <script lang="ts" setup>
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
+
 import { ref, onMounted, onUnmounted } from 'vue';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 import gsap from 'gsap';
 import confetti from 'canvas-confetti';
